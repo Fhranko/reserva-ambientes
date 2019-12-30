67fce04d065edd5069253f1438c3dd2769837240
Fhranko <francohuanaco@gmail.com>
2019-12-30 10:10:00 -0400

    Agregado seed para ver imagenes de ambientes

--- database/seeds/AmbientesSeeder.php
+++ database/seeds/AmbientesSeeder.php
@@ -12,14 +12,14 @@ class AmbientesSeeder extends Seeder
      */
     public function run()
     {
-        //
         $faker = Faker::create();
         for ($i=1; $i <= 5; $i++) {
             \DB::table('ambientes')->insert(array(
             'nombre_ambiente' => $faker->company,
             'oficina_ambiente' => $faker->streetName,
             'capacidad_ambiente' => $faker->numberBetween($min=10, $max=50),
-            'descripcion_ambiente' => $faker->text($maxNbChars = 100)
+            'descripcion_ambiente' => $faker->text($maxNbChars = 100),
+            'imagen_ambiente' => $faker->numberBetween($min=1, $max=8) . '.jpg'
             ));
         }
     }
